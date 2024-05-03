faed54d4224b36e85f3ebcb44bafe0106cc560fe
timiiify <1161341743@qq.com>
2024-05-03 12:23:30 +0800

    add redirect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,8 @@ export default defineNuxtConfig({
     routeRules: {
         '/': {
             prerender: true,
-        }
+        },
+        '/chat': { redirect: '/' }, 
     },
     app: {
         head: {
