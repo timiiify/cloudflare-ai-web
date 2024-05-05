271d46594ff25510aceb7c6a556769cb1c998608
timiiify <1161341743@qq.com>
2024-05-05 22:16:29 +0800

    add toast

--- components/ChatInput.vue
+++ components/ChatInput.vue
@@ -2,14 +2,23 @@
 const input = ref('')
 const addHistory = ref(true)
 const {openModelSelect} = useGlobalState()
-
+const toast = useToast()
 onMounted(() => {
+  toast.add({title: "如需联系上文请开启携带历史记录，图像生成请输入英文描述词"})
   addHistory.value = localStorage.getItem('addHistory') === 'true'
 })
 watch(addHistory, () => {
   localStorage.setItem('addHistory', addHistory.value.toString())
 })
-
+const handleButtonClick = () =>{
+  addHistory.value = !addHistory.value
+  if (addHistory.value === true) {
+    toast.add({ title: '已开启携带历史记录，会话可联系上文' })
+  }else{
+    toast.add({ title: '已关闭携带历史记录，会话无法联系上文' })
+  }
+  
+}
 const p = defineProps<{
   loading: boolean
   selectedModel: Model
@@ -42,7 +51,7 @@ function handleInput(e: KeyboardEvent) {
     </UButton>
     <div class="flex items-end">
       <UTooltip :text="addHistory?$t('with_history'):$t('without_history')">
-        <UButton class="m-1" @click="addHistory = !addHistory" :color="addHistory?'primary':'gray'"
+        <UButton class="m-1" @click="handleButtonClick" :color="addHistory?'primary':'gray'"
                  icon="i-heroicons-clock-solid"/>
       </UTooltip>
       <UTextarea v-model="input" :placeholder="$t('please_input_text') + '...' "
