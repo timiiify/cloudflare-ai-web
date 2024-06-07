012be7e4f681cd4709b68e861309dda637bb649e
timiiify <64126096+timiiify@users.noreply.github.com>
2024-06-07 22:40:53 +0800

    fix

--- utils/db.ts
+++ utils/db.ts
@@ -82,7 +82,7 @@ export const textGenModels: Model[] = [{
 },
 {
     id: 'gpt-4',
-    name: 'GPT4-bing',
+    name: 'GPT4-bing(维护中)',
     provider: 'openai',
     endpoint: 'api/v1/chat/completions',
     type: 'chat'
