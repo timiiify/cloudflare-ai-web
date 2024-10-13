470370739352263ea6b85fc041258ff699b7d86a
timiiify <64126096+timiiify@users.noreply.github.com>
2024-10-13 09:55:54 +0800

    Update openai.post.ts

--- server/api/auth/openai.post.ts
+++ server/api/auth/openai.post.ts
@@ -14,36 +14,15 @@ export default defineEventHandler(async (event) => {
     let authKey=''
     switch (openAIBody.model) {
         case 'claude-3-5-sonnet-20240620':
-        postUrl=`${process.env.cloud35_PROXY}/${endpoint}`
-        authKey=`Bearer ${process.env.claude35_API_KEY}`
-            break;
         case 'gpt-4-turbo':
-        postUrl=`${process.env.OPENAI4_PROXY}/${endpoint}`
-        authKey=`Bearer ${process.env.OPENAI4_API_KEY}`
-            break;
         case 'gpt-4o':
-        postUrl=`${process.env.OPENAI4o_PROXY}/${endpoint}`
-        authKey=`Bearer ${process.env.OPENAI4o_API_KEY}`
-            break;
         case 'gpt-4o-mini':
-        postUrl=`${process.env.OPENAI4omini_PROXY}/${endpoint}`
-        authKey=`Bearer ${process.env.OPENAI4omini_API_KEY}`
-            break;
         case 'gpt-4':
-        postUrl=`${process.env.BING_PROXY}/${endpoint}`
-        authKey=`Bearer ${process.env.BING_API_KEY}`
-            break;
         case 'spark':
-        postUrl=`${process.env.XUNFEI_PROXY}/${endpoint}`
-        authKey=`Bearer ${process.env.XUNFEI_KEY}`
-            break;
         case 'step':
-        postUrl=`${process.env.YUEWEN_PROXY}/${endpoint}`
-        authKey=`Bearer ${process.env.YUEWEN_KEY}`
-            break;
         case 'kimi':
-        postUrl=`${process.env.KIMI_PROXY}/${endpoint}`
-        authKey=`Bearer ${process.env.KIMI_KEY}`
+        postUrl=`${process.env.api}/${endpoint}`
+        authKey=`Bearer ${process.env.apikey}`
             break;
     }    
     const res = await fetch(postUrl, {
